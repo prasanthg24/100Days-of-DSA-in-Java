04966168443205d9ad48ce24db74c2445d7bb2e5
prasanthg24 <prasanth287471@gmail.com>
2022-01-21 22:06:59 +0530

    Code updated

---
+++ Lectures/4.Funtions/src/com/Prasanth/StringExample.java
@@ -0,0 +1,13 @@
+package com.Prasanth;
+
+public class StringExample {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println( " Mesage = " + message);
+    }
+    static  String  greet()
+    {
+        String greeting = " How are You ?";
+        return  greeting ;
+    }
+}
--- Lectures/4.Funtions/src/com/Prasanth/Sum.java
+++ Lectures/4.Funtions/src/com/Prasanth/Sum.java
@@ -6,13 +6,33 @@ import java.util.Scanner;
 public class Sum {
 
     public static void main(String[] args) {
+//
+//    int ans = sum2();
+//        System.out.println( " Sum = " + ans);
+//
+//    }
+//    /*
+//     access modifier
 
-    sum();
 
+        static int sum2()
+    {
+
+        System.out.print("Enter the n1 value = ");
+        Scanner input= new Scanner( System.in);
+        int n1 = input .nextInt();
+        System.out.print( "Enter the n2 value = ");
+        int n2 = input.nextInt();
+        int sum = n1 + n2;
+       return  sum;
     }
-    /*
-     access modifier
-     */
+    //Pass a Value of Numbers when you are calling the method in main()
+    static  int sum3( int a , int b)
+
+    {
+       int sum = a+b;
+       return sum;
+    }
     static void  sum()
     {
         System.out.print("Enter the n1 value = ");
